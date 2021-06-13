d8b8c71b6598d474b63cc1d09846e90cc608f8bd
HamburgNinja <nina.drescher@gmail.com>
2021-06-13 11:51:07 +0200

    Fixed bootstrap import

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <div className="App">
+      <div className="container">
+        <footer>
+          <a
+            href="https://github.com/HamburgNinja/react-weather-application"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open source code
+          </a>{" "}
+          by HamburgNinja
+        </footer>
+      </div>
+    </div>
+  );
 }
 
 export default App;
