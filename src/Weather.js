2a72eb2b22b3c547283bacb13793d1279d1cc793
HamburgNinja <nina.drescher@gmail.com>
2021-06-13 14:08:38 +0200

    HTML: Added more weather details

--- src/Weather.js
+++ src/Weather.js
@@ -5,15 +5,30 @@ export default function Weather() {
     <div className="Weather">
       <form>
         <div className="row">
-          <div classNAme="col-9">
-            <input type="search" placeholder="Enter city" />
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Enter city"
+              className="form-control"
+            />
           </div>
           <div className="col-3">
-            <input type="submit" value="Search" className="btn btn-info" />
+            <input type="submit" value="Search" className="btn btn-info w-80" />
           </div>
         </div>
       </form>
       <h1>Hamburg, DE</h1>
+      <p>Sunday | 14:00</p>
+      <div className="row">
+        <div className="col-2">🌤</div>
+        <div className="col-2">17°C</div>
+        <div className="col-8">partly cloudy</div>
+      </div>
+      <div className="row">
+        <div className="col-4">Feels Like 16°C</div>
+        <div className="col-4">Humidity 50%</div>
+        <div className="col-4">Wind 64km/h</div>
+      </div>
     </div>
   );
 }
